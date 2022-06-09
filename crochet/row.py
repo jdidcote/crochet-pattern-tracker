c988530e38beab5b34c4f4b38872343afcb3fda1
jdidcote <joshua.didcote@outlook.com>
2022-06-09 16:20:27 +0100

    Add class for row sequences

--- crochet/row.py
+++ crochet/row.py
@@ -8,3 +8,19 @@ class RowSequence:
     """
     A class to represent a stitch and quantity
     """
+    stitch: Stitch
+    n_stitches: int
+    color_change: bool
+
+    def __post_init__(self):
+        if self.n_stitches < 0:
+            raise ValueError("Number of stitches must be positive")
+
+
+if __name__ == '__main__':
+    from database import StitchDatabase
+    from region import Region
+
+    stitch = StitchDatabase().load_stitch(id=1, region=Region('UK'))
+    row_sequence = RowSequence(stitch, 5, False)
+    print(row_sequence)
